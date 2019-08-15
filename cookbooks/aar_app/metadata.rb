c8c709fa0e471af17cd822b1f3557c028eed7b5a
mandi walls <mandi.walls@gmail.com>
2019-08-15 18:58:46 +0000

    adding some python prereqs and epel

--- cookbooks/aar_app/metadata.rb
+++ cookbooks/aar_app/metadata.rb
@@ -4,10 +4,12 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures aar_app'
 long_description 'Installs/Configures aar_app'
-version '0.1.0'
+version '0.1.2'
 chef_version '>= 14.0'
 
 depends 'mysql'
+depends 'apache2'
+depends 'yum-epel'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
--- cookbooks/aar_app/recipes/default.rb
+++ cookbooks/aar_app/recipes/default.rb
@@ -4,5 +4,22 @@
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
 
-# create the app user on the database
-mysql -u root --socket=/var/run/mysql-AAR_DB/mysqld.sock -ptillylacto
+# need epel for some of the python stuff
+include_recipe 'yum-epel'
+
+# ensure client libraries are install for mysql
+mysql_client 'default' do
+  action :create
+end
+
+# install python bits
+package 'mod_wsgi.x86_64' do
+  action :install
+end
+
+package 'python2-pip'
+package 'MySQL-python'
+
+# get an apache server running
+apache2_install 'default'
+
