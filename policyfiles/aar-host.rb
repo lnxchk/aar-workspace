98917d57ad2a0203329de8c51d2408ee5ff64bd4
Mandi Walls <mandi.walls@gmail.com>
2019-08-28 16:42:44 -0400

    adding attrs to top-level policy

--- policyfiles/aar-host.rb
+++ policyfiles/aar-host.rb
@@ -18,3 +18,6 @@ cookbook 'aar_mysql', path: '../cookbooks/aar_mysql'
 cookbook 'aar_app', path: '../cookbooks/aar_app'
 
 include_policy 'base', policy_name: "base", policy_group: "acceptance", server: "https://manage.chef.io/organizations/mandi_learnchef"
+
+default['acceptance']['aar_app']['db_host'] = '127.0.0.1'
+default['production']['aar_app']['db_host'] = '172.31.37.51'
