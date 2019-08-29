4425c261077e65f6246417dc06788167c3c61853
Mandi Walls <mandi.walls@gmail.com>
2019-08-29 18:01:22 -0400

    adding the base policy to keep things together

--- policyfiles/base.rb
+++ policyfiles/base.rb
@@ -4,15 +4,13 @@
 # https://docs.chef.io/policyfile.html
 
 # A name that describes what the system you're building with Chef does.
-name 'aar-policy'
+name 'base'
 
 # Where to find external cookbooks:
 default_source :supermarket
 
 # run_list: chef-client will run these recipes in the order specified.
-run_list 'yum-mysql-community::mysql56','aar_mysql::default','aar_app::default'
+run_list 'os-hardening::default','base::default'
 
 # Specify a custom source for a single cookbook:
-# cookbook 'example_cookbook', path: '../cookbooks/example_cookbook'
-cookbook 'aar_mysql', path: '../cookbooks/aar_mysql'
-cookbook 'aar_app', path: '../cookbooks/aar_app'
+cookbook 'base', path: '.'
