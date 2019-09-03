aa610d2eae71acad06a8b726166e56cf1a284af5
Mandi Walls <mandi.walls@gmail.com>
2019-09-03 11:28:28 -0400

    changing name for presentation

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,7 +1,7 @@
-scaffold_policy_name="aar-policy"
-pkg_name="aar-policy"
+scaffold_policy_name="aar-host-policy"
+pkg_name="aar-host-policy"
 pkg_origin="lnxchk"
-pkg_version="0.1.0"
+pkg_version="0.1.1"
 pkg_maintainer="Mandi Walls <mandi@chef.io>"
 pkg_scaffolding="chef/scaffolding-chef-infra"
 pkg_svc_user=("root")
